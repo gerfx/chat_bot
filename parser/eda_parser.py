d430beb5dc941ffe19216d951751dde38764c1a1
eumorin <egorumorin182002@gmail.com>
2023-10-27 18:31:09 +0300

    parser added

---
+++ parser/eda_parser.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+recipes_dict = {}
+
+for i in range(1, 20):
+    url = f"https://eda.ru/recepty?page={i}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    links = soup.find_all('a', href=True)
+
+    for link in links:
+        div_tag = link.find('div')
+        if div_tag:
+            picture_tag = div_tag.find('picture')
+            if picture_tag:
+                img_tag = picture_tag.find('img', alt=True)
+                if img_tag:
+                    recipe_name = img_tag['alt']
+                    recipe_link = link['href']
+                    if recipe_name not in recipes_dict:
+                        recipes_dict[recipe_name] = "https://eda.ru" + recipe_link
